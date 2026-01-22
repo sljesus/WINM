e41c79d7ce23a2571812c667dd59cd1d8bf32fe6
JesÃºs Salazar <salazar81c@gmail.com>
2026-01-22 17:46:43 -0600

    Forzar actualizaciÃ³n completa de transactionService.js en GitHub Pages

--- js/services/transactionService.js
+++ js/services/transactionService.js
@@ -1,6 +1,6 @@
 // Servicio de Transacciones
 // Principio SOLID: Responsabilidad única - Lógica de negocio de transacciones
-// Actualizado para GitHub Pages
+// Actualizado para GitHub Pages - v2
 
 import { getSupabaseClient } from './supabaseService.js';
 import { getMonthStart, getMonthEnd } from '../utils/dateFormatter.js';
